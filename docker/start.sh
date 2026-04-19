cc56334fbdeb35dee410095bcd2a72d19874f7f1
ing-flow <foxtaka0831@gmail.com>
2026-04-19 22:12:30 +0900

    fix:start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+set -e
 
+echo "Waiting for DB..."
+
+until pg_isready -h db -U user; do
+  sleep 2
+done
+
+echo "Running migrations..."
 alembic upgrade head
 
+echo "Starting server..."
 exec gunicorn \
   -k uvicorn.workers.UvicornWorker \
   app.main:app \
